7842ce3a68dbfb2a21b0e83ace1e68e463a2d110
yinliang <yinliang@rightpaddle.com>
2020-03-28 15:12:24 +0800

    modify

--- predict.py
+++ predict.py
@@ -13,18 +13,18 @@ transform = transforms.Compose([
 
 idx_to_class = {0:'normal', 1:'pk'}
 
-def predict(model, image_name):
+def predict(use_cuda, model, image_name):
 
     test_image = Image.open(image_name)
 
     test_image_tensor = transform(test_image)
 
-    if torch.cuda.is_available():
+    if use_cuda:
         test_image_tensor = test_image_tensor.cuda()
     else:
         test_image_tensor = test_image_tensor
     test_image_tensor = Variable(torch.unsqueeze(test_image_tensor, dim=0).float(), requires_grad=False)
-    #print(test_image_tensor.shape)
+
     with torch.no_grad():
         model.eval()
         #print(model)
@@ -33,15 +33,15 @@ def predict(model, image_name):
         ps = ps / torch.sum(ps)
         topk, topclass = ps.topk(1, dim=1)
         return(idx_to_class[topclass.cpu().numpy()[0][0]], topk.cpu().numpy()[0][0])
-        #print("Prediction : ", idx_to_class[topclass.cpu().numpy()[0][0]], ", Score: ", topk.cpu().numpy()[0][0])
-
 
 
 if __name__ == '__main__':
     model_path = '/home/yinliang/works/pytorch_learn/PK/model/best_resnet.pkl'
     img_path = '/home/yinliang/works/pytorch_learn/PK/data/val/0'
     model = torch.load(model_path)
-    model.cuda()
+    use_cuda = True if torch.cuda.is_available else False
+    if use_cuda:
+        model.cuda()
     img_list = glob.glob(img_path + '/*.jpg')
     for img in img_list:
         label, score = predict(model, img)
