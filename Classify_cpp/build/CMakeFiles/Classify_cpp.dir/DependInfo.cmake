19744ed74b3e03b8c459745ac003456a66054bee
yinliang <yinliang@rightpaddle.com>
2020-05-27 16:21:08 +0800

    update

---
+++ Classify_cpp/build/CMakeFiles/Classify_cpp.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yinliang/works/pytorch_learn/Image_Classify_pytorch/Classify_cpp/classify.cpp" "/home/yinliang/works/pytorch_learn/Image_Classify_pytorch/Classify_cpp/build/CMakeFiles/Classify_cpp.dir/classify.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "AT_PARALLEL_OPENMP=1"
+  "_THP_CORE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "/home/yinliang/anaconda3/envs/pytorch2/lib/python3.6/site-packages/torch/include"
+  "/home/yinliang/anaconda3/envs/pytorch2/lib/python3.6/site-packages/torch/include/torch/csrc/api/include"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Classify_cpp/build/CMakeFiles/Classify_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Classify_cpp.dir/classify.cpp.o"
+  "Classify_cpp.pdb"
+  "Classify_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Classify_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
